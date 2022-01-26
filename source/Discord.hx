cab520de62517e743eaf40b51714eb5356f001a8
Saw <77043862+jigsaw-4277821@users.noreply.github.com>
2022-01-26 10:22:26 +0200

    Update Discord.hx

--- source/Discord.hx
+++ source/Discord.hx
@@ -9,6 +9,7 @@ class DiscordClient
 {
 	public function new()
 	{
+                #if desktop
 		trace("Discord Client starting...");
 		DiscordRpc.start({
 			clientID: "894988664453595186",
@@ -26,21 +27,26 @@ class DiscordClient
 		}
 
 		DiscordRpc.shutdown();
+                #end
 	}
 	
 	public static function shutdown()
 	{
+                #if desktop
 		DiscordRpc.shutdown();
+                #end
 	}
 	
 	static function onReady()
 	{
+                #if desktop
 		DiscordRpc.presence({
 			details: "In the Menus",
 			state: null,
 			largeImageKey: 'icon',
 			largeImageText: "itsdatrollman"
 		});
+                #end
 	}
 
 	static function onError(_code:Int, _message:String)
@@ -55,15 +61,18 @@ class DiscordClient
 
 	public static function initialize()
 	{
+                #if desktop
 		var DiscordDaemon = sys.thread.Thread.create(() ->
 		{
 			new DiscordClient();
 		});
 		trace("Discord Client initialized");
+                #end
 	}
 
 	public static function changePresence(details:String, state:Null<String>, ?smallImageKey : String, ?hasStartTimestamp : Bool, ?endTimestamp: Float)
 	{
+                #if desktop
 		var startTimestamp:Float = if(hasStartTimestamp) Date.now().getTime() else 0;
 
 		if (endTimestamp > 0)
@@ -79,9 +88,10 @@ class DiscordClient
 			smallImageKey : smallImageKey,
 			// Obtained times are in milliseconds so they are divided so Discord can use it
 			startTimestamp : Std.int(startTimestamp / 1000),
-            endTimestamp : Std.int(endTimestamp / 1000)
+                        endTimestamp : Std.int(endTimestamp / 1000)
 		});
 
 		//trace('Discord RPC Updated. Arguments: $details, $state, $smallImageKey, $hasStartTimestamp, $endTimestamp');
+                #end
 	}
 }
